691602e2acd0b0eaeedb3946d1f4eac5012fb263
Edin Jašarević <edinjasar14@gmail.com>
2022-10-06 21:20:52 +0200

    recodrs: adding a body

---
+++ ExperimentsWithRecords/PersonDto.cs
@@ -0,0 +1,23 @@
+﻿namespace ExperimentsWithRecords
+{
+    public record PersonDto(string Name)
+    {
+        public string City { get; set; }
+
+        public PersonDto() : this("Empty")
+        {
+            
+        }
+
+        public void Deconstruct(out string name, out string city)
+        {
+            name = Name;
+            city = City;
+        }
+
+        public void SomeMethod()
+        {
+
+        }
+    }
+}
