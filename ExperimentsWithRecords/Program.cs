86d7bd153216760df429eb7efc9af584cd409e99
Edin Jašarević <edinjasar14@gmail.com>
2022-10-06 21:07:39 +0200

    records internal and toString

---
+++ ExperimentsWithRecords/Program.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Dtos;
+using Shared.Enums;
+using System;
+
+namespace ExperimentsWithRecords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EventPriceDto dto = new(1, DateTimeOffset.Now, "Test", EventType.Concert, "Music hall", 80, 100);
+            Console.WriteLine($"{dto}");
+        }
+    }
+}
