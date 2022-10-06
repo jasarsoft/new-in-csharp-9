8d0c854992d2e898d19761ea06f564713f7c87fc
Edin Jašarević <edinjasar14@gmail.com>
2022-10-06 21:11:40 +0200

    record - equality

--- ExperimentsWithRecords/Program.cs
+++ ExperimentsWithRecords/Program.cs
@@ -6,10 +6,19 @@ namespace ExperimentsWithRecords
 {
     internal class Program
     {
+        public record EventPriceDtoToo(int Id, DateTimeOffset Date, string Name, EventType EventType, string Venue,
+                int PercentageSold, int TicketPrice)
+            : EventPriceDto(Id, Date, Name, EventType, Venue, PercentageSold, TicketPrice);
+
         static void Main(string[] args)
         {
-            EventPriceDto dto = new(1, DateTimeOffset.Now, "Test", EventType.Concert, "Music hall", 80, 100);
+            var now = DateTimeOffset.Now;
+            EventPriceDto dto = new(1, now, "Test", EventType.Concert, "Music hall", 80, 100);
+            EventPriceDto dto2 = new(1, now, "Test", EventType.Concert, "Music hall", 80, 100);
+            EventPriceDtoToo dtoToo = new(1, now, "Test", EventType.Concert, "Music hall", 80, 100);
             Console.WriteLine($"{dto}");
+            Console.WriteLine($"{dto == dto2}");
+            Console.WriteLine($"{dto == dtoToo}");
         }
     }
 }
